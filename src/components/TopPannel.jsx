98f1dfd67a6428195de7cc1183a19ecadcc7652b
cesar <cesar.hertz@icloud.com>
2020-10-05 19:07:13 +0800

    navigation top reminder icon added

--- src/components/TopPannel.jsx
+++ src/components/TopPannel.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Router, Link } from "@reach/router";
 
 //style
 import "./TopPannel.css";
+
+// navigation reminder icons
+import { HomeNav, EquipmentNav } from "./navReminder/TopNavBar";
 
 //photo
 import consimLogo from "./consimLogo.png";
@@ -13,8 +17,14 @@ export default function TopPannel() {
       <div className="container-consimLogo">
         <img className="consimLogo" alt="" src={consimLogo} />
       </div>
-      <div className="container-consimTitle">
+      <div className="container_consimTitle">
         <span>数字孪生施工管理平台</span>
+        <div style={{ border: "solid 1px yellow" }}>
+          <Router primary={false}>
+            <HomeNav path="/" />
+            <EquipmentNav path="Equipment" />
+          </Router>
+        </div>
       </div>
       <div className="container-companyLogo">
         <img alt="" src={imagecompany} className="companyLogo-basic" />
