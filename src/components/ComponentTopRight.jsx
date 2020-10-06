c100fa1a1efb68bfdfec69b1233c63a8ff47c0d8
cesar <cesar.hertz@icloud.com>
2020-10-06 10:20:35 +0800

    container_info_rightTop updated

--- src/components/ComponentTopRight.jsx
+++ src/components/ComponentTopRight.jsx
@@ -5,23 +5,29 @@ import "./ComponentTopRight.css";
 export default function ComponentTopRight() {
   return (
     <div className="container_info_rightTop">
-      <div className="container_info_rightTop_title">
-        <span style={{ margin: "0% 1% 0% 4%" }}>总体指标</span>{" "}
+      <div className="container_info_rightTop_head">
+        <span className="container_info_rightTop_title">总体指标</span>{" "}
       </div>
 
       <div className="container_info_rightTop_pannelOne">
-        <span style={{ margin: "1% 1% 1% 4%" }}>工地数量</span>
-        <span style={{ margin: "1% 4% 1% 1%" }}>2</span>
+        <span className="container_info_rightTop_title">工地数量</span>
+        <span className="green_numbers" style={{ margin: "1% 4% 1% 1%" }}>
+          2
+        </span>
       </div>
 
       <div className="container_info_rightTop_pannelTwo">
-        <span style={{ margin: "1% 1% 1% 4%" }}>工人总数</span>
-        <span style={{ margin: "1% 4% 1% 1%" }}>548</span>
+        <span className="container_info_rightTop_title">工人总数</span>
+        <span className="green_numbers" style={{ margin: "1% 4% 1% 1%" }}>
+          548
+        </span>
       </div>
 
       <div className="container_info_rightTop_pannelThree">
-        <span>公告栏</span>
-        <span>暂无信息</span>
+        <span className="container_info_rightTop_text">公告栏</span>
+        <div className="NoInformation">
+          <span className="container_info_rightTop_text">暂无信息</span>
+        </div>
       </div>
     </div>
   );
