4ec3e0c61350452057176bfebb8bae9f676804d1
Hanyon Wu <han4wluc@gmail.com>
2020-11-27 21:29:10 +0800

    clean unused code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.less";
 
 //pages
 import MainView from "./pages/MainView";
-import { LocationProvider, navigate } from "@reach/router";
+import { LocationProvider } from "@reach/router";
 
 //context
 import ListSitesContextProvider from "./contexts/ListSitesContext"; // will itself imports our Reducer
@@ -18,7 +18,7 @@ import "moment/locale/zh-cn";
 moment.locale("zh-cn");
 
 axios.interceptors.request.use(function (config) {
-  // Do something before request is sent
+  // add a timestamp or order to break cache
   config.params = {
     ...config.params,
     c: Date.now(),
@@ -26,77 +26,13 @@ axios.interceptors.request.use(function (config) {
   return config;
 });
 
-const LoginStatus = {
-  NotLoggedIn: 0,
-  LoggedIn: 1,
-};
-
-const tenants = [
-  {
-    credentials: {
-      username: "",
-      password: "",
-    },
-    projectName: "",
-    sites: [
-      {
-        order: 1,
-        projectShortName: "",
-        projectName: "",
-        company: "",
-        location: "",
-      },
-    ],
-  },
-];
-
 function App() {
-  let [loginStatus, setLoginStatus] = useState(LoginStatus.NotLoggedIn);
-
-  useEffect(() => {
-    const isLoggedIn = !!lockr.get("login_status");
-    if (isLoggedIn) {
-      const { projectName, sites } = !!lockr.get("current_tenant");
-      setLoginStatus(LoginStatus.LoggedIn);
-    }
-  }, [0]);
-
-  const signout = useCallback(() => {
-    setLoginStatus(LoginStatus.NotLoggedIn);
-    lockr.rm("login_status");
-  }, [setLoginStatus]);
-
-  const signin = useCallback(
-    (username, password) => {
-      // for (const tenant of tenants) {
-      //   const { credentials, sites, projectName } = tenant;
-      //   if (
-      //     username === credentials.username &&
-      //     password === credentials.password
-      //   ) {
-      //     setLoginStatus(LoginStatus.LoggedIn);
-      //     lockr.set("login_status", true);
-      //     lockr.set("current_tenant", {
-      //       projectName,
-      //       sites,
-      //     });
-      //     navigate("/");
-      //     return;
-      //   }
-      // }
-      // message.error("用户名或密码不正确");
-      lockr.set("login_status", true);
-      navigate("/");
-    },
-    [setLoginStatus]
-  );
-
   return (
     <div className="App">
       <LocationProvider>
         <TimeContextProvider>
           <ListSitesContextProvider>
-            <MainView signout={signout} />
+            <MainView />
           </ListSitesContextProvider>
         </TimeContextProvider>
       </LocationProvider>
