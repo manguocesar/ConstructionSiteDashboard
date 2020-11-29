9c2203f276002b956918c903a87dd3ae8254e458
Hanyon Wu <han4wluc@gmail.com>
2020-11-29 21:55:17 +0800

    add api call to sms pin

--- src/pages/Home/components/PinMessage.jsx
+++ src/pages/Home/components/PinMessage.jsx
@@ -1,41 +1,57 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Progress } from "antd";
-import lockr from 'lockr';
+import { Form, Input, Button, Progress, message } from "antd";
+import lockr from "lockr";
+import axios from "axios";
 
 import "./PinMessage.css";
 
 function PinMessage({ onClose }) {
+  const [inputValue, setInputValue] = useState("");
   const [status, setStatus] = useState("start");
   const [progress, setProgress] = useState(0);
 
-  const confirm = () => {
-    // TODO add api call
+  const confirm = async () => {
+    try {
+      const { data } = await axios.put(
+        "http://api.consim.cn/fudan-uni/sms-pin",
+        inputValue,
+        {
+          headers: {
+            "Content-Type": "text/plain",
+          },
+        }
+      );
 
-    lockr.set("pin_set", true)
-    setStatus("loading");
-    setProgress(2);
-    setTimeout(() => {
-      setProgress(10)
-    }, 300)
-    setTimeout(() => {
-      setProgress(40)
-    }, 1000)
-    setTimeout(() => {
-      setProgress(70)
-    }, 2000)
-    setTimeout(() => {
-      setProgress(100)
-    }, 3000)
-    setTimeout(() => {
-      setStatus('completed')
-    }, 3500)
+      lockr.set("pin_set", true);
+      setStatus("loading");
+      setProgress(2);
+      setTimeout(() => {
+        setProgress(10);
+      }, 300);
+      setTimeout(() => {
+        setProgress(40);
+      }, 1000);
+      setTimeout(() => {
+        setProgress(70);
+      }, 2000);
+      setTimeout(() => {
+        setProgress(100);
+      }, 3000);
+      setTimeout(() => {
+        setStatus("completed");
+      }, 3500);
+    } catch (error) {
+      message.error("验证码无效");
+    }
   };
 
-  if (status === 'completed') {
+  if (status === "completed") {
     return (
       <div className="pin-message-container">
         <div className="pin-message-title">数据库已更新</div>
-        <Button type="primary" onClick={onClose}>确认</Button>
+        <Button type="primary" onClick={onClose}>
+          确认
+        </Button>
       </div>
     );
   }
@@ -61,7 +77,13 @@ function PinMessage({ onClose }) {
       <div className="pin-message-title">更新巡检数据库?</div>
       <Form layout="inline">
         <Form.Item>
-          <Input placeholder="清输入安标网动态吗"></Input>
+          <Input
+            placeholder="清输入安标网动态吗"
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          ></Input>
         </Form.Item>
         <Form.Item>
           <Button type="primary" onClick={confirm}>
